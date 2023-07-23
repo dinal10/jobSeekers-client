1ff0a2dcc5b4f216a4393a06d6756b2193396258
Ilman A <128614881+Ilmanzi@users.noreply.github.com>
2023-07-23 09:34:18 +0700

    post job page styling (#24)
    Co-authored-by: Alka F <fadillaalka@gmail.com>

--- src/pages/Jobs/Recruiter/PostJob.jsx
+++ src/pages/Jobs/Recruiter/PostJob.jsx
@@ -102,111 +102,111 @@ export default function PostJob() {
   };
 
   return (
-    <div>
-      <h1>Create Job Listing</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-title"
-            >
-              Title
-            </label>
+
+    <div className="flex justify-center bg-mint">
+      <div className="bg-white rounded-lg shadow-lg p-6 m-4 w-full max-w-lg">
+        <h1 className="text-3xl font-bold my-8 text-center">Create New Job</h1>
+        <form onSubmit={handleSubmit} className="w-full">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-title"
+              >
+                Title
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="grid-title"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Job Title"
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="grid-description"
+              >
+                Description
+              </label>
+              <textarea
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+                id="grid-description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Job Description"
+              />
+            </div>
+          </div>
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Location:
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-title"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white mt-2"
               type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Job Title"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </label>
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Salary Start:
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white mt-2"
+              type="number"
+              value={salaryStart}
+              onChange={(e) => setSalaryStart(e.target.value)}
+            />
+          </label>
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Salary End:
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white mt-2"
+              type="number"
+              value={salaryEnd}
+              onChange={(e) => setSalaryEnd(e.target.value)}
+            />
+          </label>
+          <label className="mt-4">
+            Limit Date:
+            <input
+              type="date"
+              value={limitDate}
+              onChange={(e) => setLimitDate(e.target.value)}
+              className="ml-4 mt-4"
+            />
+          </label>
+
+          <div className="flex flex-col space-y-2 mt-4 w-full">
+            <h2 className="text-left">Types</h2>
+            <MultiSelect
+              className="w-full"
+              options={typeOptions}
+              value={filterTypes}
+              onChange={setFilterTypes}
+              labelledBy="Select..."
             />
           </div>
-          <div className="w-full md:w-1/2 px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-description"
-            >
-              Description
-            </label>
-            <textarea
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-              id="grid-description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="Job Description"
+
+          <div className="flex flex-col space-y-2 mt-4 w-full">
+            <h2 className="text-left">Skills</h2>
+            <MultiSelect
+              className="w-full"
+              options={skillOptions}
+              value={filterSkill}
+              onChange={setFilterSkill}
+              labelledBy="Select..."
             />
           </div>
-        </div>
-        <label
-          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          htmlFor="grid-description"
-        >
-          Location:
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-            type="text"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </label>
-        <label
-          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          htmlFor="grid-description"
-        >
-          Salary Start:
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-            type="number"
-            value={salaryStart}
-            onChange={(e) => setSalaryStart(e.target.value)}
-          />
-        </label>
-        <label
-          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          htmlFor="grid-description"
-        >
-          Salary End:
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-            type="number"
-            value={salaryEnd}
-            onChange={(e) => setSalaryEnd(e.target.value)}
-          />
-        </label>
-        <label>
-          Limit Date:
-          <input
-            type="date"
-            value={limitDate}
-            onChange={(e) => setLimitDate(e.target.value)}
-          />
-        </label>
 
-        <div className="flex flex-col space-y-2 w-80">
-          <h2 className="text-center">Types</h2>
-          <MultiSelect
-            className="w-full"
-            options={typeOptions}
-            value={filterTypes}
-            onChange={setFilterTypes}
-            labelledBy="Select..."
-          />
-        </div>
-
-        <div className="flex flex-col space-y-2 w-80">
-          <h2 className="text-center">Skills</h2>
-          <MultiSelect
-            className="w-full"
-            options={skillOptions}
-            value={filterSkill}
-            onChange={setFilterSkill}
-            labelledBy="Select..."
-          />
-        </div>
-
-        <button type="submit">Create Job Listing</button>
-      </form>
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-3 rounded-md mt-8 hover:bg-gray-800 transition duration-300"
+          >
+            Create New Job
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
